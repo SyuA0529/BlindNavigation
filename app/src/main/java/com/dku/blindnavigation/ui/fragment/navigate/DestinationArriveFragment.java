da538d379880463c413f40a67a5bd43743a7933c
NoskeLim <suhan1469@naver.com>
2023-11-14 18:29:26 +0900

    fix: ttsHelper가 초기화 되지 않은 문제 해결

--- app/src/main/java/com/dku/blindnavigation/ui/fragment/navigate/DestinationArriveFragment.java
+++ app/src/main/java/com/dku/blindnavigation/ui/fragment/navigate/DestinationArriveFragment.java
@@ -28,6 +28,7 @@ public class DestinationArriveFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_destination_arrive, container, false);
+        ttsHelper = new TTSHelper(requireActivity());
         new ViewModelProvider(requireActivity()).get(NavigateActivityViewModel.class)
                 .changeTitle("목적지 도착");
         rootView.findViewById(R.id.arriveDestToMainBT)
