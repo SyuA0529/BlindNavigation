2800794557ab52fa1ff9ee785cdef7f8df46ceec
NoskeLim <suhan1469@naver.com>
2023-12-01 09:54:54 +0900

    add: Textview 클릭시 진동 기능 추가

--- app/src/main/java/com/dku/blindnavigation/ui/fragment/setting/usage/UsageFragment2.java
+++ app/src/main/java/com/dku/blindnavigation/ui/fragment/setting/usage/UsageFragment2.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 
 import com.dku.blindnavigation.R;
 import com.dku.blindnavigation.utils.TTSHelper;
+import com.dku.blindnavigation.utils.VibrationUtil;
 
 public class UsageFragment2 extends Fragment {
 
@@ -19,10 +20,14 @@ public class UsageFragment2 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.usage_frame_2, container, false);
         TTSHelper ttsHelper = new TTSHelper(requireActivity());
-        rootView.setOnClickListener(view -> ttsHelper.speakString("네비게이션 장갑 사용법\n" +
-                "1. 서쪽은 검지, 북쪽은 중지, 동쪽은 약지, 남쪽은 소지이다.\n" +
-                "2. 한 손가락에 진동이 느껴지면 한 가지 방향을 향한다.\n" +
-                "3. 두 손가락에 진동이 느껴지면 대각선 방향을 향한다."));
+        VibrationUtil vibrationUtil = new VibrationUtil(requireActivity());
+        rootView.setOnClickListener(view -> {
+            vibrationUtil.vibrate(300);
+            ttsHelper.speakString("네비게이션 장갑 사용법\n" +
+                    "1. 서쪽은 검지, 북쪽은 중지, 동쪽은 약지, 남쪽은 소지이다.\n" +
+                    "2. 한 손가락에 진동이 느껴지면 한 가지 방향을 향한다.\n" +
+                    "3. 두 손가락에 진동이 느껴지면 대각선 방향을 향한다.");
+        });
         return rootView;
     }
 
--- app/src/main/java/com/dku/blindnavigation/ui/fragment/setting/usage/UsageFragment3.java
+++ app/src/main/java/com/dku/blindnavigation/ui/fragment/setting/usage/UsageFragment3.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 
 import com.dku.blindnavigation.R;
 import com.dku.blindnavigation.utils.TTSHelper;
+import com.dku.blindnavigation.utils.VibrationUtil;
 
 public class UsageFragment3 extends Fragment {
 
@@ -19,11 +20,15 @@ public class UsageFragment3 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.usage_frame_3, container, false);
         TTSHelper ttsHelper = new TTSHelper(requireActivity());
-        rootView.setOnClickListener(view -> ttsHelper.speakString("TTS 조절 기능\n" +
-                "1. TTS의 속도 조절은 방향키로 진행한다.\n" +
-                "2. 사용자의 설정에 맞게 0.1배속에서 2배속까지 조절이 가능하다.\n" +
-                "3. 조절을 완료한 후 저장을 누르면 해당 속도가 적용된다.\n" +
-                "4. 같은 방식으로 속도 변경 후 적용하면 된다."));
+        VibrationUtil vibrationUtil = new VibrationUtil(requireActivity());
+        rootView.setOnClickListener(view -> {
+            vibrationUtil.vibrate(300);
+            ttsHelper.speakString("TTS 조절 기능\n" +
+                    "1. TTS의 속도 조절은 방향키로 진행한다.\n" +
+                    "2. 사용자의 설정에 맞게 0.1배속에서 2배속까지 조절이 가능하다.\n" +
+                    "3. 조절을 완료한 후 저장을 누르면 해당 속도가 적용된다.\n" +
+                    "4. 같은 방식으로 속도 변경 후 적용하면 된다.");
+        });
         return rootView;
     }
 
